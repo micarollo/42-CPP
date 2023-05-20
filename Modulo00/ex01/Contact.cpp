a909bb627ee0907bddbc50d8d266289101010640
Micaela Rollo <74556711+micarollo@users.noreply.github.com>
2023-05-20 08:29:45 +0000

    setters

--- Modulo00/ex01/Contact.cpp
+++ Modulo00/ex01/Contact.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
+#include "Contact.hpp"
 
+void    Contact::set_firstname(std::string firstName)  {
+    this->firstname = firstName;
+};
+
+void    Contact::set_lastname(std::string lastName)  {
+    this->lastname = lastName;
+};
+
+void    Contact::set_nickname(std::string nickName)  {
+    this->nickname = nickName;
+};
+
+void    Contact::set_phonenum(std::string phoneNum)  {
+    this->phonenum = phoneNum;
+};
+
+void    Contact::set_darksecret(std::string darkSecret)  {
+    this->darksecret = darkSecret;
+};
--- Modulo00/ex01/Contact.hpp
+++ Modulo00/ex01/Contact.hpp
@@ -7,4 +7,11 @@ private:
     std::string nickname;
     std::string phonenum;
     std::string darksecret;
+
+public:
+    void    set_firstname(std::string firstName);
+    void    set_lastname(std::string lastName);
+    void    set_nickname(std::string nickName);
+    void    set_phonenum(std::string phoneNum);
+    void    set_darksecret(std::string darkSecret);
 };
