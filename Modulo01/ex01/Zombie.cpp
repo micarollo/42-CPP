112a7fc2e7087a0554f4c33f7e611859d5418487
Micaela Rollo <74556711+micarollo@users.noreply.github.com>
2023-06-26 17:35:29 +0000

    start

---
+++ Modulo01/ex01/Zombie.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : _name(name){
+}
+
+Zombie::~Zombie() {
+    std::cout << this->_name << " has been destroyed." << std::endl;
+}
+
+void Zombie::announce(void) {
+    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
