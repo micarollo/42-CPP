3367c240ce5dda99dbd415e861bee00ebb065cf6
Micaela Rollo <74556711+micarollo@users.noreply.github.com>
2023-06-26 15:00:56 +0000

    total for total contact that are in the phonebook

--- Modulo00/ex01/Phonebook.cpp
+++ Modulo00/ex01/Phonebook.cpp
@@ -16,6 +16,14 @@ int    Phonebook::get_num(void)  {
     return (this->count);
 };
 
+void    Phonebook::set_total(int total)  {
+    this->total = total;
+};
+
+int    Phonebook::get_total(void)  {
+    return (this->total);
+};
+
 static std::string get_input(std::string text) {
     std::string	input;
 
@@ -24,6 +32,8 @@ static std::string get_input(std::string text) {
     {
         std::cout << text << ": ";
         std::getline(std::cin, input);
+        if (!std::cin)
+            exit (1);
     }
     return (input);
 }
@@ -32,12 +42,14 @@ void	Phonebook::add_contact() {
 	std::string	input;
 
 	if (this->count == 8)
-		this->count = 0; 
+		this->count = 0;
+    if (this->total < 8)
+        this->total++;
     this->contacts[count].set_firstname(get_input("First name"));
     this->contacts[count].set_lastname(get_input("Last name"));
     this->contacts[count].set_nickname(get_input("Nickname"));
     this->contacts[count].set_phonenum(get_input("Phone number"));
-    this->contacts[count].set_darksecret(get_input("Secret number"));
+    this->contacts[count].set_darksecret(get_input("Darkest secret"));
 	this->count++;
 }
 
@@ -47,7 +59,7 @@ void Phonebook::displayContacts() {
               << std::setw(10) << "Last name" << " | "
               << std::setw(10) << "Nickname" << std::endl;
     std::cout << std::string(50, '-') << std::endl;
-    for (int i = 0; i < this->count; i++) {
+    for (int i = 0; i < this->total; i++) {
         std::cout << std::right << std::setw(10) << i + 1 << " | "
                   << std::setw(10) << std::left << (this->contacts[i].get_firstname().length() > 10 ? this->contacts[i].get_firstname().substr(0, 9) + '.' : this->contacts[i].get_firstname())
                   << " | "
@@ -58,26 +70,34 @@ void Phonebook::displayContacts() {
     }
 }
 
+// void Phonebook::searchContact(int i) {
+//     std::cout << std::right << std::setw(10) << "Index" << " | "
+//                 << std::setw(10) << "First name" << " | "
+//                 << std::setw(10) << "Last name" << " | "
+//                 << std::setw(10) << "Nickname" << std::endl;
+//     std::cout << std::string(50, '-') << std::endl;
+//     std::cout << std::right << std::setw(10) << i + 1<< " | "
+//                 << std::setw(10) << std::left << (this->contacts[i].get_firstname().length() > 10 ? this->contacts[i].get_firstname().substr(0, 9) + '.' : this->contacts[i].get_firstname())
+//                 << " | "
+//                 << std::setw(10) << std::left << (this->contacts[i].get_lastname().length() > 10 ? this->contacts[i].get_lastname().substr(0, 9) + '.' : this->contacts[i].get_lastname())
+//                 << " | "
+//                 << std::setw(10) << std::left << (this->contacts[i].get_nickname().length() > 10 ? this->contacts[i].get_nickname().substr(0, 9) + '.' : this->contacts[i].get_nickname())
+//                 << std::endl;
+// }
+
 void Phonebook::searchContact(int i) {
-    std::cout << std::right << std::setw(10) << "Index" << " | "
-                << std::setw(10) << "First name" << " | "
-                << std::setw(10) << "Last name" << " | "
-                << std::setw(10) << "Nickname" << std::endl;
-    std::cout << std::string(50, '-') << std::endl;
-    std::cout << std::right << std::setw(10) << i + 1<< " | "
-                << std::setw(10) << std::left << (this->contacts[i].get_firstname().length() > 10 ? this->contacts[i].get_firstname().substr(0, 9) + '.' : this->contacts[i].get_firstname())
-                << " | "
-                << std::setw(10) << std::left << (this->contacts[i].get_lastname().length() > 10 ? this->contacts[i].get_lastname().substr(0, 9) + '.' : this->contacts[i].get_lastname())
-                << " | "
-                << std::setw(10) << std::left << (this->contacts[i].get_nickname().length() > 10 ? this->contacts[i].get_nickname().substr(0, 9) + '.' : this->contacts[i].get_nickname())
-                << std::endl;
+    std::cout << "First name: " << this->contacts[i].get_firstname() << std::endl;
+    std::cout << "Last name: " << this->contacts[i].get_lastname() << std::endl;
+    std::cout << "Nickname: " << this->contacts[i].get_nickname() << std::endl;
+    std::cout << "Phone number: " << this->contacts[i].get_phonenum() << std::endl;
+    std::cout << "Darkest secret: " << this->contacts[i].get_darksecret() << std::endl;
 }
 
 int	Phonebook::search() {
     std::string input;
     int i;
 
-    i = this->get_num();
+    i = this->get_total();
     if (i == 0)
     {
         std::cout << "Empty Phonebook" << std::endl;
--- Modulo00/ex01/Phonebook.hpp
+++ Modulo00/ex01/Phonebook.hpp
@@ -6,12 +6,15 @@ class Phonebook {
 private:
     Contact contacts[8];
     int     count;
+    int     total;
 
 public:
     Phonebook (void);
     ~Phonebook (void);
     void    set_num(int num);
     int     get_num(void);
+    void    set_total(int total);
+    int     get_total(void);
     void	add_contact();
     void    displayContacts();
     void    searchContact(int i);
