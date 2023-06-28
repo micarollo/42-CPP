8c3acfa13d92642b70a9f4d3360b83f78b567719
Micaela Rollo <74556711+micarollo@users.noreply.github.com>
2023-06-28 12:56:04 +0000

    switch

---
+++ Modulo01/ex06/main.cpp
@@ -0,0 +1,43 @@
+#include "Harl.hpp"
+
+int option(std::string option)
+{
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (option == levels[i])
+            return (i);
+    }
+    return (4);
+}
+
+int main(int argc, char *argv[])
+{
+    Harl ha;
+
+    if (argc > 2)
+    {
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+        return (0);
+    }
+    else
+    {
+        switch (option(argv[1]))
+        {
+            case 0:
+                ha.complain("DEBUG");
+            case 1:
+                ha.complain("INFO");
+            case 2:
+                ha.complain("WARNING");
+            case 3:
+                ha.complain("ERROR");
+                break ;
+            default :
+                std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+                break ;
+        }
+    }
+    return (0);
+}
