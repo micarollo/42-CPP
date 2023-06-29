7fc73a47709e68ae0b155814251203b48d92d248
Micaela Rollo <74556711+micarollo@users.noreply.github.com>
2023-06-29 14:46:21 +0000

    claptrap

--- Modulo03/ex00/ClapTrap.hpp
+++ Modulo03/ex00/ClapTrap.hpp
@@ -1,15 +1,15 @@
-#ifndef CLAP_TRAP_HPP
-#define CLAP_TRAP_HPP
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
-#include <istream>
+#include <iostream>
 #include <string>
 
 class ClapTrap {
 private:
     std::string _name;
-    int _hit;
-    int _energy;
-    int _attack;
+    unsigned int _hit;
+    unsigned int _energy;
+    unsigned int _attack;
 
 public:
     ClapTrap(std::string name);
@@ -17,4 +17,6 @@ public:
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
-}
+};
+
+#endif
