e21e19ab0985fc05534fafd3b23f64a3e6fd0daa
Micaela Rollo <rollomicaela@gmail.com>
2023-10-12 16:50:52 +0200

    main ex01

--- Modulo03/ex01/main.cpp
+++ Modulo03/ex01/main.cpp
@@ -1,11 +1,29 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main(void)
 {
-    ClapTrap    clap1("ClapOne");
-    ClapTrap    clap2("ClapTwo");
+    ClapTrap	instanceC("Clap");
+	ScavTrap	instanceS("Scav");
 
-    clap1.attack("ClapTwo");
-    clap2.takeDamage(10);
-    clap2.beRepaired(30);
+	std::cout << std::endl;
+	
+	std::cout << instanceC.getName() << std::endl;
+	std::cout << "Hitpoints: " << instanceC.getHit() << std::endl;
+	std::cout << "Energy Points: " << instanceC.getEnergy() << std::endl;
+	std::cout << "Attack Damage: "<< instanceC.getAttack() << std::endl;
+	instanceC.attack("Enemy");
+	//	instanceC.guardGate();
+
+	std::cout << std::endl;
+
+	std::cout << instanceS.getName() << std::endl;
+	std::cout << "Hitpoints: " << instanceS.getHit() << std::endl;
+	std::cout << "Energy Points: " << instanceS.getEnergy() << std::endl;
+	std::cout << "Attack Damage: "<< instanceS.getAttack() << std::endl;
+	instanceS.attack("Enemy");
+	instanceS.guardGate();
+
+	std::cout << std::endl;
+	
+	return (0);
 }
