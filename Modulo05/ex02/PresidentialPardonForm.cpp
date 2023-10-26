16a7cc608afaab2afaaa0c7feb9b60a7ed8df763
Micaela Rollo <rollomicaela@gmail.com>
2023-10-26 15:32:37 +0200

    presidential class

---
+++ Modulo05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,32 @@
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(void)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(std::string target) : AForm("PresidentialPardonForm", 25, 5), _target(target)
+{
+    std::cout << "Constructor called" << std::endl;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm(void)
+{
+    std::cout << "Default destructor called" << std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm const & cp) : AForm(cp), _target(cp._target)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+PresidentialPardonForm & PresidentialPardonForm::operator=(PresidentialPardonForm const & presForm)
+{
+    (void)presForm;
+    return (*this);
+}
+
+std::string const & PresidentialPardonForm::getTarget(void) const
+{
+    return (this->_target);
+}
---
+++ Modulo05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,24 @@
+#ifndef PRESIDENTIAL_PARDON_FORM_HPP
+#define PRESIDENTIAL_PARDON_FORM_HPP
+
+#include <iostream>
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+    public:
+        PresidentialPardonForm(std::string target);
+        ~PresidentialPardonForm(void);
+        PresidentialPardonForm (PresidentialPardonForm const &cp);
+
+        PresidentialPardonForm &operator=(PresidentialPardonForm const & presForm);
+
+        std::string const & getTarget(void) const;
+
+    private:
+        const std::string _target;
+        PresidentialPardonForm(void);
+};
+
+#endif
