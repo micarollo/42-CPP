99f94a3b4781c8ff2a42a9a72f56b71bd9e09ba2
Micaela Rollo <rollomicaela@gmail.com>
2023-10-30 11:16:00 +0100

    Intern not ready and Makeile

---
+++ Modulo05/ex03/Intern.cpp
@@ -0,0 +1,40 @@
+#include "Intern.hpp"
+
+Intern::Intern(void)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Intern::~Intern(void)
+{
+    std::cout << "Default destructor called" << std::endl;
+}
+
+Intern::Intern(Intern const &cp)
+{
+    std::cout << "Constructor called" << std::endl;
+}
+
+Intern & Intern::operator=(Intern const & intern)
+{
+    (void)intern;
+    return (*this); 
+}
+
+AForm* Intern::makeForm(std::string name, std::string target)
+{
+    ShrubberyCreationForm	sh		= ShrubberyCreationForm(target);
+	RobotomyRequestForm		rb		= RobotomyRequestForm(target);
+	PresidentialPardonForm	pr	= PresidentialPardonForm(target);
+    AForm*	forms[] = {&sh, &rb, &pr};
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (forms[i]->getName() == name)
+        {
+            std::cout << "Intern creates " << name << std::endl;
+            return(forms[i]);
+        }
+    }
+    return (NULL);
+}
---
+++ Modulo05/ex03/Intern.hpp
@@ -0,0 +1,21 @@
+#ifndef INTERN_HPP
+#define INTER_HPP
+
+# include "AForm.hpp"
+# include "ShrubberyCreationForm.hpp"
+# include "RobotomyRequestForm.hpp"
+# include "PresidentialPardonForm.hpp"
+
+class Intern
+{
+public:
+    Intern(void);
+    ~Intern(void);
+    Intern(Intern const &cp);
+
+    Intern &operator=(Intern const & intern);
+
+    AForm* makeForm(std::string name, std::string target);
+};
+
+#endif
