7cf183bcf076c90a87eecfdfa7efd84424550e41
Micaela Rollo <rollomicaela@gmail.com>
2023-11-02 13:22:02 +0100

    08-00not ready

--- Modulo08/easyfind.hpp
+++ Modulo08/easyfind.hpp
@@ -1,17 +1,20 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
 template <typename T>
-int easyfind(const T& container, int value) 
+typename T::iterator easyfind(const T& container, int value) 
 {
-    auto it = std::find(container.begin(), container.end(), value);
+    typename T::iterator it;
+
+    it = std::find(container.begin(), container.end(), value);
     if (it != container.end()) {
-        return std::distance(container.begin(), it); // Found the value, return its index
+        throw std::exception();
     } else {
-        // If not found, you can throw an exception or return an error value
-        throw std::runtime_error("Value not found");
-        // Alternatively, you can return -1 as an error value:
-        // return -1;
+        return (it);
     }
 }
 
