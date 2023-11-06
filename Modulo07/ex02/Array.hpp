e0aa87b47f5308015de87933067d7ae81822c381
Micaela Rollo <rollomicaela@gmail.com>
2023-11-06 16:12:43 +0100

    format

--- Modulo07/ex02/Array.hpp
+++ Modulo07/ex02/Array.hpp
@@ -1,13 +1,13 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-template<typename T>
+template <typename T>
 
 class Array
 {
 public:
-    Array<T>(void) : _ar(new T[0]), _size(0) {};
-    
+    Array<T>(void) : _ar(new T[0]), _size(0){};
+
     ~Array<T>(void)
     {
         delete[] this->_ar;
@@ -21,11 +21,11 @@ public:
     Array<T>(Array<T> const &cp) : _size(cp._size)
     {
         _ar = new T[_size];
-        for (unsigned int i = 0; i  < _size; i++)
+        for (unsigned int i = 0; i < _size; i++)
             _ar[i] = cp._ar[i];
     }
 
-    Array<T> & operator=(Array<T> const &src)
+    Array<T> &operator=(Array<T> const &src)
     {
         if (&src == this)
             return (*this);
@@ -44,25 +44,23 @@ public:
         return (_ar[i]);
     }
 
-    class	OutOfRange : public std::exception
-	{
-	public:
-		virtual const char *	what() const throw()
+    class OutOfRange : public std::exception
+    {
+    public:
+        virtual const char *what() const throw()
         {
             return ("Array index out of range");
         }
-	};
+    };
 
     size_t size(void) const
     {
         return (this->_size);
     }
 
-
 private:
     T *_ar;
     unsigned int _size;
-
 };
 
 #endif
