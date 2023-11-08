167a81f2c58e54b9e991f8822ebc5771e191edce
Micaela Rollo <rollomicaela@gmail.com>
2023-11-08 17:50:14 +0100

    07-01 iter const

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,7 +1,15 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template< typename T> void iter(T *ar, int size, void f(const T &))
+template <typename T>
+void iter(T *ar, int size, void f(const T &))
+{
+    for (int i = 0; i < size; i++)
+        f(ar[i]);
+}
+
+template <typename T>
+void iter(T *ar, int size, void f(T &))
 {
     for (int i = 0; i < size; i++)
         f(ar[i]);
