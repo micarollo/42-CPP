375c26b1c115ba4ea937d5b1600d8ac4eda4b5db
micarollo <rollomicaela@gmail.com>
2024-01-23 10:32:42 +0100

    compiling

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -2,14 +2,7 @@
 #define ITER_HPP
 
 template <typename T>
-void iter(T *ar, int size, void f(const T &))
-{
-    for (int i = 0; i < size; i++)
-        f(ar[i]);
-}
-
-template <typename T>
-void iter(T *ar, int size, void f(T &))
+void iter(T *ar, int size, void (f)(const T &))
 {
     for (int i = 0; i < size; i++)
         f(ar[i]);
