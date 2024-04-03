1f32068fd9d97ce8464af59b4fe1f9d4090be14d
micarollo <rollomicaela@gmail.com>
2024-04-03 14:40:10 +0200

    function working

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -4,160 +4,131 @@ PmergeMe::PmergeMe() {}
 
 PmergeMe::~PmergeMe() {}
 
-void PmergeMe::mergeVector(std::vector<int> &arr, int left, int middle, int right)
-{
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
-
-    std::vector<int> L(n1), R(n2);
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[middle + 1 + j];
-
-    int i = 0, j = 0, k = left;
-
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void PmergeMe::mergeSortVector(std::vector<int> &arr, int left, int right)
-{
-    if (left < right)
-    {
-        int middle = left + (right - left) / 2;
-
-        if (middle - left + 1 <= 10)
-        {
-            for (int i = left + 1; i <= right; i++)
-            {
-                int key = arr[i];
-                int j = i - 1;
-                while (j >= left && arr[j] > key)
-                {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-                arr[j + 1] = key;
-            }
-        }
-        else
-        {
-            mergeSortVector(arr, left, middle);
-            mergeSortVector(arr, middle + 1, right);
-            mergeVector(arr, left, middle, right);
-        }
-    }
-}
-
-void PmergeMe::mergeList(std::list<int>::iterator left, std::list<int>::iterator middle, std::list<int>::iterator right)
-{
-    std::list<int> l(left, middle);
-    std::list<int> r(middle, right);
-
-    std::list<int>::iterator it = left;
-
-    while (!l.empty() && !r.empty())
-    {
-        if (l.front() <= r.front())
-        {
-            *it = l.front();
-            l.pop_front();
-        }
-        else
-        {
-            *it = r.front();
-            r.pop_front();
-        }
-        ++it;
-    }
-
-    while (!l.empty())
-    {
-        *it = l.front();
-        l.pop_front();
-        ++it;
-    }
-
-    while (!r.empty())
-    {
-        *it = r.front();
-        r.pop_front();
-        ++it;
-    }
-}
-
-void PmergeMe::mergeSortList(std::list<int> &arr, std::list<int>::iterator left, std::list<int>::iterator right)
-{
-    if (distance(left, right) > 1)
-    {
-        std::list<int>::iterator middle = left;
-        advance(middle, distance(left, right) / 2);
-
-        if (distance(left, middle) <= 10)
-        {
-            for (std::list<int>::iterator i = next(left); i != right; ++i)
-            {
-                int key = *i;
-                std::list<int>::iterator j = prev(i);
-                while (j != prev(left) && *j > key)
-                {
-                    *next(j) = *j;
-                    --j;
-                }
-                *next(j) = key;
-            }
-        }
-        else
-        {
-            mergeSortList(arr, left, middle);
-            mergeSortList(arr, middle, right);
-            mergeList(left, middle, right);
-        }
-    }
-}
-
 void PmergeMe::printVector(const std::vector<int> &arr)
 {
-    for (std::vector<int>::const_iterator it = arr.begin(); it != arr.end(); ++it)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+	for (std::vector<int>::const_iterator it = arr.begin(); it != arr.end(); ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 }
 
 void PmergeMe::printList(const std::list<int> &arr)
 {
-    for (std::list<int>::const_iterator it = arr.begin(); it != arr.end(); ++it)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+	for (std::list<int>::const_iterator it = arr.begin(); it != arr.end(); ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+std::list<int> PmergeMe::mergeList(std::list<int> left, std::list<int> right)
+{
+	std::list<int> mergedList;
+	std::list<int>::iterator l, r;
+
+	l = left.begin();
+	r = right.begin();
+
+	while (l != left.end() && r != right.end())
+	{
+		if (*l < *r)
+		{
+			mergedList.insert(mergedList.end(), *l);
+			l++;
+		}
+		else
+		{
+			mergedList.insert(mergedList.end(), *r);
+			r++;
+		}
+	}
+	mergedList.insert(mergedList.end(), l, left.end());
+	mergedList.insert(mergedList.end(), r, right.end());
+
+	return mergedList;
+}
+
+std::list<int> PmergeMe::insertSortlist(std::list<int> list)
+{
+	if (list.size() <= 10)
+	{
+		std::list<int>::iterator it, j;
+
+		for (it = std::next(list.begin(), 1); it != list.end(); ++it)
+		{
+			int key = *it;
+			j = it;
+
+			while (j != list.begin() && *std::prev(j) > key)
+			{
+				*j = *(std::prev(j));
+				--j;
+			}
+			*j = key;
+		}
+		return list;
+	}
+	else
+	{
+		unsigned long mid = list.size() / 2;
+		std::list<int> left(list.begin(), std::next(list.begin(), mid));
+		std::list<int> right(std::next(list.begin(), mid), list.end());
+
+		return (mergeList(insertSortlist(left), insertSortlist(right)));
+	}
+}
+
+std::vector<int> PmergeMe::mergeVector(std::vector<int> left, std::vector<int> right)
+{
+	std::vector<int> mergedVector;
+	std::vector<int>::iterator l, r;
+
+	l = left.begin();
+	r = right.begin();
+
+	while (l != left.end() && r != right.end())
+	{
+		if (*l < *r)
+		{
+			mergedVector.push_back(*l);
+			l++;
+		}
+		else
+		{
+			mergedVector.push_back(*r);
+			r++;
+		}
+	}
+	mergedVector.insert(mergedVector.end(), l, left.end());
+	mergedVector.insert(mergedVector.end(), r, right.end());
+
+	return mergedVector;
+}
+
+std::vector<int> PmergeMe::insertSortVector(std::vector<int> v)
+{
+	if (v.size() <= 10)
+	{
+		for (unsigned long i = 1; i < v.size(); i++)
+		{
+			int key = v[i];
+			int j = i - 1;
+			while (j > -1 && v[j] > key)
+			{
+				v[j + 1] = v[j];
+				j--;
+			}
+			v[j + 1] = key;
+		}
+		return v;
+	}
+	else
+	{
+		unsigned long mid = v.size() / 2;
+		std::vector<int> left(v.begin(), v.begin() + mid);
+		std::vector<int> right(v.begin() + mid, v.end());
+
+		return mergeVector(insertSortVector(left), insertSortVector(right));
+	}
 }
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -11,8 +11,15 @@ public:
     ~PmergeMe();
     void mergeVector(std::vector<int> &arr, int left, int middle, int right);
     void mergeSortVector(std::vector<int> &arr, int left, int right);
-    void mergeList(std::list<int>::iterator left, std::list<int>::iterator middle, std::list<int>::iterator right);
-    void mergeSortList(std::list<int> &arr, std::list<int>::iterator left, std::list<int>::iterator right);
+    // void mergeList(std::list<int>::iterator left, std::list<int>::iterator middle, std::list<int>::iterator right);
+    // void mergeSortList(std::list<int> &arr, std::list<int>::iterator left, std::list<int>::iterator right);
     void printVector(const std::vector<int> &arr);
     void printList(const std::list<int> &arr);
+
+    std::list<int> insertSortlist( std::list<int> list );
+    std::list<int> mergeList( std::list<int> left, std::list<int> right );
+    // std::list<int> insertSortList(std::list<int> list);
+    std::vector<int> insertSortVector( std::vector<int> vector );
+    std::vector<int> mergeVector( std::vector<int> left, std::vector<int> right );
+    // std::vector<int> insertSortVector( std::vector<int> vector );
 };
