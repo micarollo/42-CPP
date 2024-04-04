a6ebc29e40b0ab9e1b813eb0beba1e6b002d2b40
micarollo <rollomicaela@gmail.com>
2024-04-04 10:20:52 +0200

    main like subject

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,8 +1,4 @@
 #include "PmergeMe.hpp"
-// #include <iostream>
-// #include <vector>
-// #include <list>
-// #include <cstdlib>
 #include <ctime>
 
 int main(int argc, char *argv[])
@@ -19,7 +15,6 @@ int main(int argc, char *argv[])
     std::vector<int> vecSorted;
     PmergeMe p;
 
-    // Parse the input sequence and populate both vector and list
     for (int i = 1; i < argc; i++)
     {
         int num = atoi(argv[i]);
@@ -32,29 +27,27 @@ int main(int argc, char *argv[])
         arrList.push_back(num);
     }
 
-    // Measure time for sorting using vector
+    // Measure time vector
     clock_t startVector = clock();
-    // p.mergeSortVector(arrVector, 0, arrVector.size() - 1);
     vecSorted = p.insertSortVector(arrVector);
     clock_t endVector = clock();
     double vectorTime = static_cast<double>(endVector - startVector) / CLOCKS_PER_SEC * 1000;
 
-    // Measure time for sorting using list
+    //Measure time list
     clock_t startList = clock();
-    // p.mergeSortList(arrList, arrList.begin(), --arrList.end());
     listSorted = p.insertSortlist(arrList);
     clock_t endList = clock();
     double listTime = static_cast<double>(endList - startList) / CLOCKS_PER_SEC * 1000;
 
-    // Print sorted sequences
-    std::cout << "Sorted sequence using vector: ";
+    // Print before after
+    std::cout << "Before: ";
+    p.printVector(arrVector);
+    std::cout << "After: ";
     p.printVector(vecSorted);
-    std::cout << "Sorted sequence using list: ";
-    p.printList(listSorted);
 
-    // Print time comparisons
-    std::cout << "Time taken to sort using vector: " << vectorTime << " seconds" << std::endl;
-    std::cout << "Time taken to sort using list: " << listTime << " seconds" << std::endl;
+    // Print time
+    std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector: " << vectorTime << " seconds" << std::endl;
+    std::cout << "Time to process a range of " << argc - 1 << " elements with std::list: " << listTime << " seconds" << std::endl;
 
     return 0;
 }
